94c5432b777ca5e0cdaa152b42332633c12188b6
Sebastian Tremblay <tremblay.se@northeastern.edu>
2024-01-04 15:40:13 -0800

    Hiding sensitive data

--- v1/Main.py
+++ v1/Main.py
@@ -1,11 +1,12 @@
+import os
+import credentials
+
 from Controller import Controller
 
 if __name__ == "__main__":
-    KEY = "723831a338d34248846f5d53e4896dae"
-    ENDPOINT = "https://delt-facial-recognition-v1.cognitiveservices.azure.com/"
     TRAINING_DATA_PATH = "./training_data"
     
-    controller = Controller(KEY, ENDPOINT)
+    controller = Controller(credentials.API_KEY, credentials.API_ENDPOINT)
     controller.train_model(TRAINING_DATA_PATH)
     
     video_path = "./examples/test3.MOV"
